46620c1e47af3d92df32c6132a2baf312a557f1a
NadezdaNN <pnnov@yandex.ru>
2024-08-14 22:54:44 +0300

    main

--- main.py
+++ main.py
@@ -5,19 +5,16 @@ from Weather import func_weather
 from PyQt5.QtCore import QSettings
 
 app = Flask(__name__, static_folder="static")
-
 settings = QSettings("my_city_name")  
-city_dict = {}
 
 @app.route('/count/<name_city>', methods=['POST'])
 def count(name_city): # получить кол-во обращений по названию города 
     if request.method == 'POST':        
-        city_dict = settings.value("city_name_for_api")                 
+        city_dict = settings.value("city_name_for_api")                  
         try:            
             return json.dumps({str(name_city): city_dict[name_city]}), 200
-        except:            
-            return json.dumps({"Нет такого города": 0}), 200
-    
+        except:                
+            return json.dumps({"Нет такого города": 0}), 200    
 
 @app.route('/get_forecast', methods=['GET', 'POST'])
 def get_forecast():     
@@ -25,22 +22,23 @@ def get_forecast():
         name_city = request.form['name_city']        
         settings.setValue("my_city_name", str(name_city))  
         city_dict = settings.value("city_name_for_api") 
+        
         try: 
             city_dict[name_city] = city_dict[name_city] + 1            
         except:            
             city_dict[name_city] = 1          
         finally:             
-            settings.setValue("city_name_for_api", city_dict)            
+            settings.setValue("city_name_for_api", city_dict)                      
             pred = func_weather(name_city) # получение прогноза               
             return json.dumps({'pred': pred[0], 'pred2': pred[1], 'pred3': pred[2], 'pred4': pred[3]})
     
 
-@app.route('/start')  
+@app.route('/')  
 def start():               
-    my_city = settings.value("my_city_name")
-    return render_template('weatherForecast.html', city=my_city)
+    my_city = settings.value("my_city_name")    
+    return render_template('weatherForecast.html', city=str(my_city))
     
 
 if __name__ == "__main__":          
-    webbrowser.open_new("http://127.0.0.1:5000/start")
+    webbrowser.open_new("http://127.0.0.1:5000/")
     app.run() # debug=True
