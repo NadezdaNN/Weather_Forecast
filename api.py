85b227c19b723aa8bf728ec6fedf9667730871c5
NadezdaNN <pnnov@yandex.ru>
2024-08-15 10:20:00 +0300

    finish

--- api.py
+++ api.py
@@ -1,8 +1,10 @@
 import requests
 import json
-        
-headers = {
-    "Content-Type": "application/json"
-}
-success = requests.post('http://127.0.0.1:5000/count/Moscou', headers=headers).json()
-print(success)
+from sys import argv
+
+name_city = argv
+
+if __name__ == '__main__':    
+    for i in name_city[1::]:
+        success = requests.post(f'http://127.0.0.1:5000/count/{i}').json()
+        print(success)
